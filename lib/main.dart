9b2fe229b1bdae23cf8f7039daf5ac8fd6aaa5f7
bhsh200 <117628732+bhsh200@users.noreply.github.com>
2024-01-27 23:52:58 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/app.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Spotify Clone',
+    debugShowCheckedModeBanner: false,
+    home: MyApp()
+  ));
+}
---
+++ lib/views/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // UI design
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedLabelStyle: TextStyle(color: Colors.white),
+        backgroundColor: Colors.black45,
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.white), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search, color: Colors.white), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.library_add_outlined, color: Colors.white), label: 'Your Library'),
+        ]),
+    );
+  }
+}
